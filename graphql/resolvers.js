a177db31511355b0f446102db6227e260ab5466b
Jasom Park <zeipar@gmail.com>
2020-10-08 22:12:13 -0700

    Created mutation to add a movie.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,12 @@
-import { getMovies, getById } from "./db.js";
+import { getMovies, getById, addMovie } from "./db.js";
 
 const resolvers = {
   Query: {
     movies: () => getMovies(),
     movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
   },
 };
 
