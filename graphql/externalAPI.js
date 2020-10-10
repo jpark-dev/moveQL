9bb0f3c365b2b115a5959ab352c3078ad2d6e77e
Jasom Park <zeipar@gmail.com>
2020-10-09 21:32:29 -0700

    Added a check counter to make accurate URL to retrieve.

--- graphql/externalAPI.js
+++ graphql/externalAPI.js
@@ -3,12 +3,17 @@ const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
   let REQUEST_URL = API_URL;
+  let isChecked = false;
 
   if (limit > 0) {
     REQUEST_URL += `limit=${limit}`;
+    isChecked = true;
   }
   if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
+    if (isChecked) {
+      REQUEST_URL += "&";
+    }
+    REQUEST_URL += `minimum_rating=${rating}`;
   }
 
   console.log(REQUEST_URL);
