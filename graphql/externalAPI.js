d510ca1168f42711240702d97605fe6445b780e7
Jasom Park <zeipar@gmail.com>
2020-10-24 17:56:59 -0700

    Refactored.

--- graphql/externalAPI.js
+++ graphql/externalAPI.js
@@ -1,26 +1,20 @@
 import axios from "axios";
-const LIST_MOVIES_URL = "https://yts.am/api/v2/list_movies.json?";
-const MOVIE_DETAILS_URL = "https://yts.am/api/v2/movie_details.json";
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json?`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 
 export const getMovies = async (limit, rating) => {
   let REQUEST_URL = LIST_MOVIES_URL;
-  let isChecked = false;
-
-  if (limit > 0) {
-    REQUEST_URL += `limit=${limit}`;
-    isChecked = true;
-  }
-  if (rating > 0) {
-    if (isChecked) {
-      REQUEST_URL += "&";
-    }
-    REQUEST_URL += `minimum_rating=${rating}`;
-  }
   const {
     data: {
       data: { movies },
     },
-  } = await axios(REQUEST_URL);
+  } = await axios(REQUEST_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
   return movies;
 };
 
