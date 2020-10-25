218f233f5982872d8f12061a8ccab932de4ef13a
Jasom Park <zeipar@gmail.com>
2020-10-24 18:00:20 -0700

    Created getSuggestions and updated resolver.

--- graphql/externalAPI.js
+++ graphql/externalAPI.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const BASE_URL = "https://yts.am/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json?`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+cost MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
   let REQUEST_URL = LIST_MOVIES_URL;
@@ -30,3 +31,16 @@ export const getMovie = async id => {
   });
   return movie;
 };
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: {movies}
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movies;
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,11 @@
 import { addMovie, deleteMovie } from "./db.js";
-import { getMovies, getMovie } from "./externalAPI.js";
+import { getMovies, getMovie, getSuggestions } from "./externalAPI.js";
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
     movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
   Mutation: {
     addMovie: (_, { name, score }) => addMovie(name, score),
